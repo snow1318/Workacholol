d8caf725ad85c2d74a833871d6fe2ab85982bd7d
snow1318 <snojachittu@gmail.com>
2025-02-22 11:49:58 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-// App.js
 import React from 'react';
-import Login from './page/login';
-function App() {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+
+// Importing pages (create them next)
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Cart from './pages/Cart';
+import BrandDeals from './pages/BrandDeals';
+import Keyboards from './pages/Keyboards';
+import Headsets from './pages/Headsets';
+import Speakers from './pages/Speakers';
+
+const App = () => {
   return (
-    <div>
-      <Login/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/BrandDeals" element={<BrandDeals />} />
+        <Route path="/Keyboards" element={<Keyboards />} />
+        <Route path="/Headsets" element={<Headsets />} />
+        <Route path="/Speakers" element={<Speakers />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
 
